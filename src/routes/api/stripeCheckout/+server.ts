a2689f12d91c9b46ce327b07201f9810be724500
Idea Phakpaphon <ideapakpaphon@gmail.com>
2024-03-14 16:07:21 +0700

    Add dynamic env set success_url and cancel_url for stripeCheckout

--- src/routes/api/stripeCheckout/+server.ts
+++ src/routes/api/stripeCheckout/+server.ts
@@ -41,8 +41,8 @@ export const POST: RequestHandler = async ({ locals, request, cookies }) => {
         metadata: {
             user_id: userId,
         },
-        success_url: "https://localhost:5173/success",
-        cancel_url: "https://localhost:5173/cancel",
+        success_url: `${import.meta.env.VITE_WEBURL}/success`,
+        cancel_url: `${import.meta.env.VITE_WEBURL}/cancel`,
     });
     const metadata2 = session.metadata.user_id;
     console.log(metadata2)
